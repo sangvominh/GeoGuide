4df5307be602b7407d65541c747dae291f32a9bb
Minh Sang <vosang20102005@gmail.com>
2026-04-06 23:36:05 +0700

    fix bug startup flow
    commit 7425779a0e09537cd0f2edad059df0c9d960f63e
    Author: Minh Sang <vosang20102005@gmail.com>
    Date:   Mon Apr 6 22:31:36 2026 +0700
    
        fixbug startup flow

--- MauiApp1/MauiProgram.cs
+++ MauiApp1/MauiProgram.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Logging;
 using MauiApp1.Services;
+using Npgsql;
 using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace MauiApp1
@@ -19,9 +20,28 @@ namespace MauiApp1
                     fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIcons");
                 });
 
+            var dbConnectionString = Environment.GetEnvironmentVariable("POI_DB_CONNECTION");
+            if (string.IsNullOrWhiteSpace(dbConnectionString))
+            {
+                var csb = new NpgsqlConnectionStringBuilder
+                {
+                    Host = Environment.GetEnvironmentVariable("POI_DB_HOST") ?? "localhost",
+                    Port = int.TryParse(Environment.GetEnvironmentVariable("POI_DB_PORT"), out var port) ? port : 5432,
+                    Database = Environment.GetEnvironmentVariable("POI_DB_NAME") ?? "POIcsharpApp",
+                    Username = Environment.GetEnvironmentVariable("POI_DB_USER") ?? "postgres",
+                    Password = Environment.GetEnvironmentVariable("POI_DB_PASSWORD") ?? "1234",
+                    SslMode = SslMode.Disable,
+                    TrustServerCertificate = true
+                };
+
+                dbConnectionString = csb.ConnectionString;
+            }
+
             // Register services
             builder.Services.AddSingleton<LocationService>();
             builder.Services.AddSingleton<StartupWarmupService>();
+            builder.Services.AddSingleton(new PostgresDbOptions { ConnectionString = dbConnectionString });
+            builder.Services.AddSingleton<PostgresPoiService>();
 
             // Register pages
             builder.Services.AddTransient<Pages.SplashPermissionPage>();
