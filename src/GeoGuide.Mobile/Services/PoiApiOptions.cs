508da748989ab611dea69a814a67bfdb88e6b62a
Minh Sang <vosang20102005@gmail.com>
2026-04-16 13:06:16 +0700

    mobile: align default poi api url with develop backend

--- src/GeoGuide.Mobile/Services/PoiApiOptions.cs
+++ src/GeoGuide.Mobile/Services/PoiApiOptions.cs
@@ -2,7 +2,7 @@ namespace MauiApp1.Services;
 
 public class PoiApiOptions
 {
-    public string BaseUrl { get; set; } = "http://10.0.2.2:8080";
+    public string BaseUrl { get; set; } = "http://10.0.2.2:5005";
     public int TimeoutSeconds { get; set; } = 10;
     public bool EnablePlaybackLogs { get; set; } = true;
 }
